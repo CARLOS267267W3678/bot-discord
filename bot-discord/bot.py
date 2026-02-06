b5cdd0ece9dc241bbae7724b2c83b2308e8595f0
Felipe <SAMPAIOMANUGOMESB@gmail.com>
2026-02-06 02:34:28 -0300

    corrige uso do TOKEN via variável de ambiente

--- bot-discord/bot.py
+++ bot-discord/bot.py
@@ -17,4 +17,5 @@ async def on_ready():
     print(f"🤖 Bot online como {bot.user}")
     await bot.load_extension("cogs.fila")
 
-bot.run("MTQ2OTA4OTg3MDAxNzMzNTUyMg.G0ADHX.F3vwkuJqJU4ZjOPb_mUK7fD_Y3oykGZcLtbKC0")
+
+bot.run(TOKEN)
